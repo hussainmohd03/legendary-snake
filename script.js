2194e055a77e71dead69ecaaf2ae6d5953bfea31
hussainmohd03 <hussainmohdali03@gmail.com>
2025-06-30 11:20:47 +0300

    create 'moveSnake' function & and interval to move the snake every 170ms

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const gameSection = document.querySelector('#game-section')
 
 let snakeLocation = [{ row: 15, column: 15 }]
 
+let snakeDirection = 'right'
+
 const createGame = () => {
-  displaySnake()
+  gameSection.innerHTML = ''
   displayFood()
+  displaySnake()
 }
 
 const displaySnake = () => {
@@ -20,9 +23,43 @@ const displaySnake = () => {
 const displayFood = () => {
   const food = document.createElement('div')
   food.setAttribute('class', 'food')
-  food.style.gridColumn = Math.floor(Math.random() * 30) + 1
-  food.style.gridRow = Math.floor(Math.random() * 30) + 1
+  food.style.gridColumn = foodPosition.column
+  food.style.gridRow = foodPosition.row
   gameSection.appendChild(food)
 }
 
-createGame()
+const generateFoodPosition = () => {
+  const row = Math.floor(Math.random() * 30) + 1
+  const column = Math.floor(Math.random() * 30) + 1
+  return { row, column }
+}
+
+const moveSnake = () => {
+  let snakeHead = { ...snakeLocation[0] }
+
+  // snakeLocation.pop()
+
+  switch (snakeDirection) {
+    case 'right':
+      snakeHead.column++
+      break
+    case 'left':
+      snakeHead.column--
+      break
+    case 'up':
+      snakeHead.row--
+      break
+    case 'down':
+      snakeHead.row++
+      break
+  }
+
+  snakeLocation.unshift(snakeHead)
+}
+
+let foodPosition = generateFoodPosition()
+
+setInterval(() => {
+  moveSnake()
+  createGame()
+}, 170)
